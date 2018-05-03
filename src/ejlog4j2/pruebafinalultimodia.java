e0e545a935f5e5380f80e46341971ca1941ba452
israel <israel.touchcorn@hotmail.com>
2018-05-03 16:28:55 +0200

    esto es la prueba finalkhjlkj

---
+++ src/ejlog4j2/pruebafinalultimodia.java
@@ -0,0 +1,18 @@
+package ejlog4j2;
+
+/**
+ * Created by ISRAEL-PC on 03/05/2018.
+ */
+public class pruebafinalultimodia {
+    <component name="NewModuleRootManager" inherit-compiler-output="true">
+    <exclude-output />
+    <content url="file://$MODULE_DIR$">
+    <sourceFolder url="file://$MODULE_DIR$/src" isTestSource="false" />
+    <sourceFolder url="file://$MODULE_DIR$" type="java-test-resource" />
+    </content>
+    <orderEntry type="inheritedJdk" />
+    <orderEntry type="sourceFolder" forTests="false" />
+    <orderEntry type="library" exported="" name="Log4j" level="project" />
+    <orderEntry type="library" name="log4j-api-2.11.0-javadoc" level="application" />
+    </component>
+}
